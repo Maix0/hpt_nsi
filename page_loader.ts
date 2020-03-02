d2eb9bd2bb4080b806f253df69ff1c01fb47fd7f
Maix0 <39835848+Maix0@users.noreply.github.com>
2020-03-02 21:35:27 +0100

    added base files

---
+++ page_loader.ts
@@ -0,0 +1,40 @@
+function addPage(link: string, icon: string, title: string) {
+    let html = `<li class="nav-item" data-page="${link}"><a href="#" class="nav-link" ><i class="material-icons">${icon}</i><span class="link-text">${title}</span></a></li>`;
+    document.querySelector(".navbar-nav") !.innerHTML += html;
+    console.log({
+        link,
+        icon,
+        title
+    });
+}
+
+const pages = [{
+    link: "./acceuil.html",
+    icon: "home",
+    title: "Accueil"
+}, {
+    link: "./apps.html",
+    icon: "apps",
+    title: "Apps"
+}];
+const iframe = document.querySelector(".page_viewer") as HTMLIFrameElement;
+
+function main() {
+    pages.forEach(({
+        link,
+        icon,
+        title
+    }) => {
+        addPage(link, icon, title);
+    });
+    document.querySelectorAll(".navbar-nav .nav-item").forEach(el => {
+        (el as HTMLLIElement).addEventListener("click", function (this) {
+            iframe.src = this.dataset.page!;
+            document.querySelector("title")!.innerText = "HPT - " + iframe.contentWindow!.document.querySelector("title")!.innerText;
+        })
+    })
+}
+
+
+
+main()
