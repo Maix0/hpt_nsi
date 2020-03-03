359e2be9c91e4237d8a109c1c26bf50ec3388c3f
maix0 <maix522@gmail.com>
2020-03-03 14:43:54 +0100

    commented some more code and moved the pages to own folder

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # Logement Pous Tous
 Un site web qui vise a montrer comment se loger.
-
-
--- page_loader.ts
+++ page_loader.ts
@@ -1,36 +1,50 @@
+// La fonction addPage permet de rajouter un lien dans la navbar a droite de l'écran
+// Cela va generer le code HTML requis avec les information donnée
+// et ensuite le rajouter a la fin du code existant dans une balise avec la classe "navbar-nav"
 function addPage(link: string, icon: string, title: string) {
     let html = `<li class="nav-item" data-page="${link}"><a href="#" class="nav-link" ><i class="material-icons">${icon}</i><span class="link-text">${title}</span></a></li>`;
     document.querySelector(".navbar-nav") !.innerHTML += html;
-    console.log({
-        link,
-        icon,
-        title
-    });
 }
 
+// la constante "pages" permet de definir les liens qui seront afficher dans la navbar
+// La fonction "main" va parcourire tous les elements de cette liste, lancer la fonction addPage()
 const pages = [{
-    link: "./accueil.html",
+    link: "./pages/accueil.html",
     icon: "home",
     title: "Accueil"
 }, {
-    link: "./apps.html",
+    link: "./pages/apps.html",
     icon: "apps",
     title: "Apps"
 }];
+// Une referance a la balise <iframe> qui a une classe "page_viewer"
 const iframe = document.querySelector(".page_viewer") as HTMLIFrameElement;
 
+// La fonction main est la fontion qui sera lancée a la fin du chargement de la page
+// Appelle la fonction "AddPage()" avec les donnée de chaque element de la liste "pages".
+// Apres cela, elle va ajouter un event "onclick" sur tout les elements ajouter, 
+// qui va changer la proprité "src" de l'iframe , ce qui va changer la page affichée dans l'<iframe>
+
 function main() {
+    // passez a travers tous les elements de la liste "Page" 
     pages.forEach(({
         link,
         icon,
         title
     }) => {
+        // Lance la foncion addPage() avec les valeurs de l'element de la liste
         addPage(link, icon, title);
     });
+    // Recupere une liste de tous les elements qui on la classe "nav-item"
+    // Ajoute un eventListener sur l'evenement "click" qui changera la page afficher 
+    // quand l'element en question sera cliquer dessu. 
     document.querySelectorAll(".navbar-nav .nav-item").forEach(el => {
         (el as HTMLLIElement).addEventListener("click", function (this) {
             iframe.src = this.dataset.page!;
-            document.querySelector("title") !.innerText = "HPT - " + iframe.contentWindow!.document.querySelector("title") !.innerText;
+            // Fait en sorte que la fonction si dessou se lance apres 500ms, pour attendre que la page charge
+            setTimeout(() => {
+                document.querySelector("title") !.innerText = "HPT - " + iframe.contentWindow!.document.querySelector("title") !.innerText;
+            }, 500)
         })
     })
 }
